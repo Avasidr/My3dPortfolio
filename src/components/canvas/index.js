4a928f245ad61b222403fa348d905b165219e04d
Noa <coxava9449@gmail.com>
2025-02-13 23:31:20 +0100

    changes in about section

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,5 @@
 import ContactModel from "./ContactModel";
-import BallCanvas from "./Ball";
 import DeveloperModel from "./DeveloperModel";
 import StarsCanvas from "./Stars";
 
-export { ContactModel, BallCanvas, DeveloperModel, StarsCanvas };
+export { ContactModel, DeveloperModel, StarsCanvas };
--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,7 @@
-import {
-  ContactModel,
-  BallCanvas,
-  DeveloperModel,
-  StarsCanvas,
-} from "./canvas";
+import { ContactModel, DeveloperModel, StarsCanvas } from "./canvas";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
-import Tech from "./Tech";
 import Experience from "./Experience";
 import Works from "./Works";
 import Contact from "./Contact";
@@ -16,12 +10,10 @@ export {
   Hero,
   Navbar,
   About,
-  Tech,
   Experience,
   Works,
   Contact,
   ContactModel,
-  BallCanvas,
   DeveloperModel,
   StarsCanvas,
 };
